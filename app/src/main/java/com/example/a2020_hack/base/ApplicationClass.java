4201d197f11c092e3933e3517b52b3ca89ac30c5
jangsm <tmdalsy@naver.com>
2020-01-16 11:10:38 +0900

    기본 1차

---
+++ app/src/main/java/com/example/a2020_hack/base/ApplicationClass.java
@@ -0,0 +1,11 @@
+package com.example.a2020_hack.base;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationClass extends Application {
+}
